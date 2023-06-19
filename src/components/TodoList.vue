f154988440d7bae5ad83dc2d6fda74af5d81d90b
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-19 15:55:56 +0900

    add mapGetters in Todolist

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        <div v-for="todo in allTodos" :key="todo.id">
+            <TodoItem />
+    </div>
     </div>
 </template>
 <script>
-
+    import { mapGetters } from "vuex";
     import TodoItem from "@/components/TodoItem";
     export default {
         name: 'TodoList',
         components: {
             TodoItem
-        }
-
+        },
+        computed: {
+            ...mapGetters(['allTodos'])
+        },
     }
 </script>
 <style scoped>
