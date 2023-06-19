9f7aed7fabbbc6af29e15eb96d8cdb048edae0fa
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-19 16:07:12 +0900

    add deletetodo in TodoItem

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -5,17 +5,21 @@
             <h3 class="col-6">{{ todo.title }}</h3>
             <div class="col-4">
                 <button class="btn btn-primary mx-2 ">Edit</button>
-                <button class="btn btn-danger ">Delete</button>
+                <button @click="deleteTodo(todo.id)" class="btn btn-danger ">Delete</button>
             </div>
             
         </div>
     </div>
 </template>
 <script>
+    import { mapActions } from "vuex";
     export default {
         name: 'TodoItem',
         props: {
             todo: {},
+        },
+        methods: {
+            ...mapActions(["deleteTodo"]),
         },
     }
 </script>
