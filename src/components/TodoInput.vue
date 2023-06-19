d69e8cdf269956dccbbf4bbe18725ee5748f9999
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-19 15:50:21 +0900

    add text change event

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="d-flex">
-            <input v-model="todoText" type="text" class="form-control todoinput" placeholder="Enter todo" v-on:keydown.enter="submitTask" >
+            <input @change="todoTextChange($event)" v-bind:value="todoText" type="text" class="form-control todoinput" placeholder="Enter todo"  >
             <button @click="addTodoI" class="btn btn-success rounded-0">ADD</button>
         </div>
     </div>
@@ -19,11 +19,16 @@ import { v1 } from 'uuid';
         },
         methods: {
             ...mapActions(["addTodo"]),
+            todoTextChange(e){
+                // this.todoText = e.target.value;
+                console.log("todoTextChange ", this.todoText);
+            },
             addTodoI(){
                 this.addTodo({
                     id: v1(),
                     title: this.todoText,
-                })
+                });
+                this.todoText = '';
             }
         }
     }
