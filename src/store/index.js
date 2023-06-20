1a5de7fb5464ddfb29239cf5d33e7338d912dec6
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-20 10:05:55 +0900

    install vue-persistedstate and usecreateVuexPersistedState

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { createVuexPersistedState } from "vue-persistedstate";
 
 Vue.use(Vuex)
+
 const STORAGE_KEY = 'vue-todo-appv2';
+
 export default new Vuex.Store({
+    plugins: [
+        createVuexPersistedState({
+          key: STORAGE_KEY,
+          storage: window.localStorage,
+          whiteList: [],
+          blackList: [],
+        }),
+      ],
     state: {
-        // todos: [
-            
-         
-        // ]
-        todos: JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
-        
+        todos: []        
     },
     
     getters: {
@@ -33,22 +39,15 @@ export default new Vuex.Store({
     mutations: {
         add_todo(state, todo){
             state.todos.push(todo);
-            
-            console.log(todo);
-            console.log(state.todos);
-            localStorage.setItem(STORAGE_KEY, JSON.stringify(state.todos));
-
         },
         delete_todo(state, id){
             state.todos = state.todos.filter(todo=> todo.id != id);
-            localStorage.setItem(STORAGE_KEY, JSON.stringify(state.todos)); 
         },
         update_todo(state, todo){
             let index = state.todos.findIndex(t=> t.id == todo.id);
             console.log(index);
             if(index != -1){
-                state.todos[index] = todo; 
-                localStorage.setItem(STORAGE_KEY, JSON.stringify(state.todos));             
+                state.todos[index] = todo;            
             }
             
         },
